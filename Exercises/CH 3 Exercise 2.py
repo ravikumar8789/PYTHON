24d7e7237715233a2ecb8c15562b561ca5a100dd
ravikumar8789 <123381432+ravikumar8789@users.noreply.github.com>
2023-09-10 15:15:17 +0530

    fixed small error in exercise 2 ch 3

--- Exercises/CH 3 Exercise 2.py
+++ Exercises/CH 3 Exercise 2.py
@@ -1,8 +1,8 @@
 name= input("Enter your name : ")
 age = int(input("Enter your age : "))
 # name.lower()
-name1=name.lower
-if(name[0]=='a' and age>10):
+# name1=name.lower
+if((name[0]=='a' or name[0]=='A') and age>=10):
     print("you can watch coco movie")
 else:
     print("you cannot watch coco movie") 

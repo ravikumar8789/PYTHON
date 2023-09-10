4de14c94daeb423da041926c372bb98192dd3dd0
ravikumar8789 <123381432+ravikumar8789@users.noreply.github.com>
2023-09-10 21:03:19 +0530

    loop

--- for_loop.py
+++ for_loop.py
@@ -5,12 +5,12 @@
 #     print(f"hello python {i}")
 
 
-name = input("Enter your name : ")
-temp = ""
-for i in range(len(name)):
-    if(name[i] not in temp):
-        print(f"{name[i]}:{name.count(name[i])}")
-        temp= name[i]+temp
+# name = input("Enter your name : ")
+# temp = ""
+# for i in range(len(name)):
+#     if(name[i] not in temp):
+#         print(f"{name[i]}:{name.count(name[i])}")
+#         temp= name[i]+temp
 
 # name = input("Enter your name: ")
 # temp = ""
@@ -21,4 +21,8 @@ for i in range(len(name)):
 #         print(f"{name[i]}:{count}")
 
 
-    
+number= input("Enter a number : ")
+sum=0
+for i in number:
+    sum= int(i)+sum
+print(sum)

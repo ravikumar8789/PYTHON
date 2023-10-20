2b1be8aaaba0115dc910372d41adeb324158c2b3
ravikumar8789 <123381432+ravikumar8789@users.noreply.github.com>
2023-10-20 21:27:36 +0530

    Exercises and practices

--- exception_handling.py
+++ exception_handling.py
@@ -1,11 +1,15 @@
 while True:
     try:
         age= int(input("Enter your age : "))
-        break
     except ValueError:
         print('you have entered non integer value')
     except:
         print('unexpected error')
+    else:
+        print(f'you have entered {age}')
+        break
+    finally:
+        print('this is finally block....')
 
 
 if age<18:
